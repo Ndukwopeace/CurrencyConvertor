67fa32c849dc13762e77a7e95d136ea3aa075b98
ndukwopeace <ndukwopeace@gmail.com>
2024-04-19 16:16:45 +0100

    another commit

--- Client/src/components/header.jsx
+++ Client/src/components/header.jsx
@@ -1,10 +1,17 @@
 import react , { useState } from 'react'
+import '../assets/styles/Header.css'
+import { Box } from '@mui/material'
+
 
 const Header = () =>{
 
     return (
         <>
-            header
+
+            <Box sx={{backgroundColor:'#211535'}}>
+            </Box>
+            
+
         </>
     )
 
