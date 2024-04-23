63f8e9772e860231fdccf4b3d4c24d275547e2f4
mark <markgolah69@gmail.com>
2024-04-23 14:28:25 +0100

    added footer this time

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React from 'react';
+import appleLogo from '../assets/images/SVG (1).png';
+import googleLogo from '../assets/images/SVG.png';
+import trustPilot from '../assets/images/SVG1.png';
+import star from '../assets/images/SVG 3.png';
+import greenStar from '../assets/images/Vector (5).png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-[#F7F8F9] flex justify-center  gap-[5rem] py-[2rem]'>
+        {/* logos */}
+        {/* appleLogo */}
+        <div className='flex items-center gap-[0.7rem]'>
+          <img src={appleLogo} alt="" />
+          {/* ratings */}
+          <div className='flex flex-col gap-[0.5rem] items-center'>
+            {/* stats */}
+            <div className='flex gap-[0.2rem]' >
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+            </div>
+
+            <p className='text-xs'><span className='font-[600] '>4.5/5</span>, 2.2k ratings</p>
+          </div>
+        </div>
+
+         {/* googleLogo */}
+         <div className='flex items-center gap-[0.7rem]'>
+          <img src={googleLogo} alt="" />
+          {/* ratings */}
+          <div className='flex flex-col gap-[0.5rem] items-center'>
+            {/* stats */}
+            <div className='flex gap-[0.2rem]' >
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+            </div>
+
+            <p className='text-xs'><span className='font-[600] '>3.8/5</span>, 90.8k ratings</p>
+          </div>
+        </div>
+
+        {/* trustPilot */}
+        <div className='flex items-center gap-[0.7rem]'>
+          <img src={trustPilot} alt="" />
+          {/* ratings */}
+          <div className='flex flex-col gap-[0.5rem] items-center'>
+            {/* stats */}
+            <div className='flex gap-[0.2rem]' >
+              <img src={greenStar} alt="" />
+              <img src={greenStar} alt="" />
+              <img src={greenStar} alt="" />
+              <img src={greenStar} alt="" />
+              <img src={greenStar} alt="" />
+            </div>
+
+            <p className='text-xs'><span className='font-[600] '>4.7/5</span>, 41.5k ratings</p>
+          </div>
+        </div>
+    </div>
   )
 }
 
--- Client/src/components/Recom.jsx
+++ Client/src/components/Recom.jsx
@@ -7,29 +7,73 @@ import logo1 from '../assets/images/SVG1.png'
 const Recom = () => {
   return (
     <>
-    <div className='bg-grey border border-black text-center flex flex-col'>
-        <h3>Recommended by over 40,000 verified customers</h3>
+      <div className='bg-grey border border-black  flex flex-col gap-[2rem] pt-[1rem] pb-[4.5rem]'>
+        <h3 className='self-center text-[#0A146E] font-[600] text-[1.3rem]'>Recommended by over 40,000 verified customers</h3>
 
-        <div className='flex  '>
-            <div className='bg-[#060D4C] text-white text-[1.1rem] flex flex-col items-center gap-[0.5rem] p-[2rem] rounded-lg'>
-                  <p>'Great'</p>
-                  <div className='flex '>
-                    <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] '/>   
-                    <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] '/>    
-                    <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] '/>    
-                    <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] '/>    
-                    <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] '/>    
-                  </div>
-                  <p>Based on 68,513 reviews</p>
-                <div className='flex gap-[0.2rem] '>
-                  <img src={star} alt="" />
-                  <span className='relative top-[0.5rem]'>Trustpilot</span>
-                </div>
+        <div className='flex gap-[3rem] justify-center'>
+          {/* first */}
+          <div className='bg-[#060D4C] text-white text-[1.1rem] w-[16rem] flex flex-col items-center justify-center gap-[0.5rem]  rounded-lg'>
+            <p>'Great'</p>
+            <div className='flex '>
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
             </div>
+            <p>Based on 68,513 reviews</p>
+            <div className='flex gap-[0.2rem] '>
+              <img src={star} alt="" />
+              <span className='relative top-[0.5rem]'>Trustpilot</span>
+            </div>
+          </div>
+          {/* two */}
+          <div className='text-[1.1rem] border shadow-md flex flex-col gap-[2rem] p-[1rem] w-[16rem] rounded-lg'>
+            <div className='flex flex-col '>
+            <div className='flex'>
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+            </div>
+            <p className='font-[600]'>'I'd like to have a French
+              communication</p>
+            <p className='text-[0.9rem]'>I'd like to have a French
+              communication</p>
+
+            </div>
+            <div className='flex flex-col gap-[0.2rem] text-xs'>
+              <p >Johanne Fréchette</p>
+              <span className='text-[#F89623] '>22 hours, 57 minutes ago</span>
+            </div>
+          </div>
+
+  {/* three */}
+  <div className='text-[1.1rem] border shadow-md flex flex-col gap-[5rem] p-[1rem] w-[16rem] rounded-lg'>
+            <div className='flex flex-col '>
+            <div className='flex'>
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+              <img src={star} alt="" className='w-[1.5rem] h-[1.5rem] ' />
+            </div>
+            <p className='font-[600]'>Good one more time</p>
+            <p className='text-[0.9rem]'>Good one more time</p>
+
+            </div>
+            <div className='flex flex-col gap-[0.2rem] text-xs'>
+              <p >FREDERIC COULLON</p>
+              <span className='text-[#F89623] '>99 hours, 58 minutes ago</span>
+            </div>
+          </div>
+
+
         </div>
-        
-    </div>
-    
+
+      </div>
+
     </>
   )
 }
