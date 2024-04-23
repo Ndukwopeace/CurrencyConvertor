5353bd3f90fbdb8ecdea9a2a427fd86e3303f312
mark <markgolah69@gmail.com>
2024-04-23 16:01:00 +0100

    my next commit

---
+++ Client/src/components/CurrencyConverter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CurrencyConverter = () => {
+  return (
+    <div>
+
+        <form action="">
+            <div>
+                <label htmlFor="amount">Amount</label>
+                <input type="number" name='amount'/>
+            </div>
+            
+        </form>
+
+    </div>
+  )
+}
+
+export default CurrencyConverter
--- Client/src/components/Form.jsx
+++ Client/src/components/Form.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React from 'react';
+import coins from '../assets/images/SVG (2).png';
+import arrowHead from '../assets/images/Vector (1).png';
+import graph from '../assets/images/Vector (2).png';
+import alerts from '../assets/images/SVG (3).png';
+import CurrencyConverter from './CurrencyConverter';
+
+
 
 const Form = () => {
   return (
-    <div>
-        <div>
+    <div className='flex justify-center '>
+        <div className='w-[60%] border border-black   '>
+        {/* Navbar */}
+          <div className='grid grid-cols-4 hover:cursor-pointer bg-[#F0F5FA] py-[0.7rem] '>
+            <div className='flex items-center gap-2 justify-center focus:bg-white'>
+              <img src={coins} alt="" />
+              <p>Convert</p>
+            </div>
 
-          <div>
-            
+            <div className='flex items-center gap-2 justify-center'>
+              <img src={arrowHead} alt="" />
+              <p>Send</p>
+            </div>
+
+            <div className='flex items-center gap-2 justify-center'>
+              <img src={graph} alt="" />
+              <p>Graphics</p>
+            </div>
+
+            <div className='flex items-center gap-2 justify-center'>
+              <img src={alerts} alt="" />
+              <p>Alerts</p>
+            </div>
           </div>
 
+        {/* currency converter  */}
+
+        <CurrencyConverter/>
 
 
         </div>
